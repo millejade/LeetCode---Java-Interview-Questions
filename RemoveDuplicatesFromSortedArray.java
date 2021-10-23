2c8d54feb509617ba94bdb3a988348d2a62679b4
Mille Jade Cabrera <36434363+millejade@users.noreply.github.com>
2021-10-23 21:53:56 +0800

    Update RemoveDuplicatesFromSortedArray.java

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -40,7 +40,7 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 
 Constraints:
 
-0 <= nums.length <= 3 * 104
+0 <= nums.length <= 3 * 10^4
 -100 <= nums[i] <= 100
 nums is sorted in non-decreasing order.
 
