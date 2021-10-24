10f53d4499633f8e4cc79a0fc1cba0ff36274a44
Mille Jade Cabrera <36434363+millejade@users.noreply.github.com>
2021-10-24 10:51:48 +0800

    Update MergeSortedArray.java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -41,8 +41,8 @@ public class MergeSortedArray {
         
         int nums1[] = {1,2,3,0,0,0};
         int nums2[] = {2,5,6};
-        int m = 0;
-        int n = 0;
+        int m = 3;
+        int n = 3;
         
         merge(nums1, m, nums2, n);
         
@@ -50,10 +50,48 @@ public class MergeSortedArray {
     
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         
+     
+     /*
+     we've got to start with rightmost index with values that are greater 0
+     i = m - 1 >>> last index of nums1[] array
+     j = n - 1 >>> last index of nums2[] array
+     k = m + n - 1 >>> last index of new array which will be stored in nums1[] array    
+     
+     store the new merge sorted array in nums1[] array
+     
+     IF
+     ~ i < 0 :  since 'i' was already negative, we can't use this as an index to get some element in an array, so we'll just use 'j' to become an index
+     ~ value of index 'j' is GREATER THAN value of index 'i' >>> assign value of index 'j' to the specific element (k) of NEW ARRAY (nums1[])...
+     ...then right after that, we need to remove this value of index 'j' by decrementing 'j'.
+     
+     ELSE
+     ~ value of index 'i' is GREATER THAN value of index 'j' >>> assign value of index 'i' to the specific element (k) of NEW ARRAY (nums1[])...
+     ...then right after that, we need to remove this value of index 'i' by decrementing 'i'
+     
+     */
         for(int i = m - 1, j = n - 1, k = m + n - 1; k >= 0 && j >= 0; k--){
             nums1[k] = (i < 0 || nums1[i] < nums2[j]) ? nums2[j--] : nums1[i--];
+          /*
+          
+                            if(i < 0 || nums1[i] < nums2[j]){
+                              nums1[k] = nums2[j--]; //j-- here 'j--' is still 'j', it is assigned before it decrements ( nums1[k] = nums2[j] )
+                            }else{
+                              nums1[k] = nums1[i--]
+                            }
+                            
+                            example: (why do we need the those conditions from FOR LOOP and IF STATEMENT)
+                            m = 1
+                            n = 0
+                            i = 0, j = -1, k = 0 >>> do nothing in merge method, just print the nums1[] array since nums2[] array got NO ELEMENTS/EMPTY ARRAY
+                            
+                            m = 0
+                            n = 1
+                            i = -1, j = 0, k = 0 >>> transfer the elements of nums2[] array into nums1[] array since nums1[] array got NO ELEMENTS/EMPTY ARRAY
+                            
+          
+          */
         }
         
-    }
+    } 
     
 }
